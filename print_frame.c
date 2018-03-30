d0d7c66087b03da00e2e7f36613b909f1623ed9a
Aaron Nacar <Aaron>
2018-03-30 09:20:54 -0500

    This is print frame

---
+++ print_frame.c
@@ -0,0 +1,14 @@
+# include "stdio.h"
+# include "test_data.h"
+int main ()
+    {
+        int x;
+
+        for(x = 0; x < 80; x++)
+        {
+        printf ("%c", a_frame[0][x]);
+        "/n";
+        }
+
+    return 0;
+    }
