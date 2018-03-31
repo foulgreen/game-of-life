6abff01f1772f781e75f55e6cf25821651a635f9
Aaron <excel9976@hotmail.com>
2018-03-31 13:33:17 -0500

    print frame 2nd attempt

--- print_frame.c
+++ print_frame.c
@@ -2,13 +2,18 @@
 # include "test_data.h"
 int main ()
     {
-        int x;
-
-        for(x = 0; x < 80; x++)
+        int x = 401;
+        //printf("%lu\n", sizeof(a_frame));
+        while(x >= 0)
         {
-        printf ("%c", a_frame[0][x]);
-        "/n";
-        }
+            printf ("%c", a_frame[0][x]);
+            if(x %20 == 0)
+                {
+                    printf("\n");
+                }
+
+            x--;
+        };
 
     return 0;
     }
