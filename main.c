c119b2766c876ff0ba14d27486a814ba4b224dbb
Aaron <excel9976@hotmail.com>
2018-05-17 20:27:36 -0500

    Fix calls to print_frame()

--- main.c
+++ main.c
@@ -8,13 +8,12 @@ int main(void)
 	for(int i = 0; i < 20; ++i) {
 		a[i] = a_frame[i];
 	}
-	print_frame(a, 20, 20);
+	print_frame(stdout, a, 20, 20);
 	process_frame(&a, 20, 20);
-	print_frame(a, 20, 20);
+	print_frame(stdout, a, 20, 20);
 	process_frame(&a, 20, 20);
-	print_frame(a, 20, 20);
+	print_frame(stdout, a, 20, 20);
 	process_frame(&a, 20, 20);
-	print_frame(a, 20, 20);
-	process_frame(&a, 20, 20);
+	print_frame(stdout, a, 20, 20);
 
 }
